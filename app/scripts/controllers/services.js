b218243722179af8aa9abcaea4b4aaa77c338b5a
Lee Braddock <lee.braddock@cru.org>
2013-06-06 14:31:22 -0400

    Refactored services.js

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -1,13 +1,33 @@
 'use strict';
 
-angular.module('relayServicesApp')
-  .controller('ServicesCtrl', function ($scope,$http) {
-              $scope.emplid = "";
-              $scope.response = "";
+var protocols = { unsecure : 'http://' , secure : 'https://' };
+var proxys = { yes : 'localhost:9292/', no : ''};
+var hosts = { test : 'tlidma21.ccci.org:8083', prod : 'signin.ccci.ccci.org' };
+var updaterest = '/pshr-listener/rest/employee/update/';
 
-  $scope.reconcileEmployee = function () {
-              $http.get('http://localhost:9292/tlidma21.ccci.org:8083/pshr-listener/rest/user/update/'+ $scope.emplid).success(function(data) { $scope.response = data });
-              $scope.emplid = "";
-              $scope.response = "";
+var protocol = protocols.unsecure;
+var proxy = proxys.yes;
+var host = hosts.test;
+
+var url = protocol + proxy + host + updaterest;
+
+var servicesFactory =
+        function ($http) {
+            var factory = {};
+            factory.reconcileEmployee = function(emplid, $scope) {
+                $http.put(url, 'emplid=' + emplid).success(function(data) { $scope.response = data });
             };
-  });
+            return factory;
+        };
+
+var servicesController =
+        function ($scope, ServicesFactory) {
+          $scope.reconcileEmployee = function()
+          {
+            ServicesFactory.reconcileEmployee($scope.emplid, $scope);
+          };
+         };
+
+angular.module('relayServicesApp')
+.factory('ServicesFactory', servicesFactory)
+.controller('ServicesCtrl', servicesController);
