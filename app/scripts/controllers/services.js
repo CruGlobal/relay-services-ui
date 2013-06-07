2badea472ad856a0ddf0d8a040af7cfea985bd64
Lee Braddock <lee.braddock@cru.org>
2013-06-07 14:01:56 -0400

    Gruntfile.js - reordering grunt registered build tasks in to meet certain requirements related to deploying css services.js - inlining factory and controller definitions since minifiy causes problems when definining them individually.  Also, now utilzing relative url in http.

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -1,34 +1,18 @@
 'use strict';
 
-var protocols = { unsecure : 'http://' , secure : 'https://' };
-var proxys = { yes : 'localhost:9292/', no : ''};
-var hosts = { test : 'tlidma21.ccci.org:8083', prod : 'signin.ccci.ccci.org' };
-var updaterest = '/pshr-listener/rest/employee/update/';
-
-var protocol = protocols.unsecure;
-var proxy = proxys.yes;
-var host = hosts.test;
-
-var url = protocol + proxy + host + updaterest;
-
-var servicesFactory =
-        function ($http) {
-            var factory = {};
-            factory.reconcileEmployee = function(emplid, $scope) {
-                $http.put(url, 'emplid=' + emplid).success(function(data) { $scope.response = data; });
-            };
-            return factory;
-        };
-
-var servicesController =
-        function ($scope, ServicesFactory) {
-          $scope.reconcileEmployee = function()
-          {
-            $scope.response = 'reconciliation attempt in progress ...';
-            ServicesFactory.reconcileEmployee($scope.emplid, $scope);
-          };
-         };
-
 angular.module('relayServicesApp')
-.factory('ServicesFactory', servicesFactory)
-.controller('ServicesCtrl', servicesController);
+.factory('ServicesFactory', function ($http) {
+                                        var factory = {};
+                                        var updaterest = '/pshr-listener/rest/employee/update/';
+                                        factory.reconcileEmployee = function(emplid, $scope) {
+                                            $http.put(updaterest, 'emplid=' + emplid).success(function(data) { $scope.response = data; });
+                                        };
+                                        return factory;
+                                    })
+.controller('ServicesCtrl', function ($scope, ServicesFactory) {
+                                      $scope.reconcileEmployee = function()
+                                      {
+                                        $scope.response = 'reconciliation in progress ...';
+                                        ServicesFactory.reconcileEmployee($scope.emplid, $scope);
+                                      };
+                                     });
