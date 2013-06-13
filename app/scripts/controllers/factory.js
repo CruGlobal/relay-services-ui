21fdcca68b7c321a2372496a1f5fed4f7696b7c5
Lee Braddock <Lee_Braddock@lhjf-begins-l.net.ccci.org>
2013-06-13 16:36:16 -0400

    refactoring factory and removing reference to test url

--- app/scripts/controllers/factory.js
+++ app/scripts/controllers/factory.js
@@ -1,14 +1,8 @@
-var servicesFactory =
-    function ($http) {
-        var factory = {};
-
-        var updaterest = 'http://localhost:9292/tlidma21.ccci.org:8083/pshr-listener/rest/employee/update/';
-
-        factory.reconcileEmployee = function(emplid, $scope) {
+relayServicesApplication.factory('ServicesFactory', function ($http) {
+    return {
+     reconcileEmployee: function(emplid, $scope) {
+            var updaterest = '/pshr-listener/rest/employee/update/';
             $http.put(updaterest, 'emplid=' + emplid).success(function(data) { $scope.response = data; });
-        };
-
-        return factory;
+            }
     };
-
-relayServicesApplication.factory('ServicesFactory', servicesFactory);
+});
