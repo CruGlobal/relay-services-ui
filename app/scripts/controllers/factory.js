5a3bfba3f989ecbc7ab2bbd4ebfdb40add958f8d
Lee Braddock <lee.braddock@cru.org>
2013-06-19 13:02:24 -0400

    fixed merge conflicet from revert to fb1fac57dfccc8b16b5f5d030cf7e98a031bffdc

--- app/scripts/controllers/factory.js
+++ app/scripts/controllers/factory.js
@@ -1,10 +1,6 @@
 'use strict';
 
-<<<<<<< HEAD
-relayServicesApplication.factory('ReconciliationService', [ '$http', function (http) {
-=======
 relayServicesApplication.factory('ServicesFactory', function ($http) {
->>>>>>> parent of fb1fac5... refactoring
     return {
             reconcileEmployee: function(emplid, $scope) {
 
--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -1,17 +1,9 @@
 'use strict';
 
-<<<<<<< HEAD
-relayServicesApplication.controller('ServicesCtrl', ['$scope', 'ReconciliationService', function (model, service) {
-    model.reconcileEmployee =
-        function() {
-            model.response = 'reconciliation of employee id ' + model.emplid + ' in progress ...';
-            service.reconcileEmployee(model.emplid, model);
-=======
 relayServicesApplication.controller('ServicesCtrl', function ($scope, ServicesFactory) {
     $scope.reconcileEmployee =
         function() {
             $scope.response = 'reconciliation of employee id ' + $scope.emplid + ' in progress ...';
             ServicesFactory.reconcileEmployee($scope.emplid, $scope);
->>>>>>> parent of fb1fac5... refactoring
         };
     }]);
